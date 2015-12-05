2d468f0823bb099c028407061b7b723fffb548bd
Gareth Clay <gareth.clay@gmail.com>
2015-12-05 18:03:38 +0000

    Day 3 part 2

--- src/advent_of_code/day_3.clj
+++ src/advent_of_code/day_3.clj
@@ -16,8 +16,20 @@
   [ [[last-x last-y] & _ :as visited] [move-x move-y] ]
   (cons [(+ last-x move-x) (+ last-y move-y)] visited))
 
+(defn houses-visited
+  [movements]
+  (distinct (reduce apply-movement [[0 0]] movements)))
+
 (defn part-1
   [& [data]]
   (let [data (or data instructions-from-file)
         movements (map ->movement (seq data))]
-    (count (distinct (reduce apply-movement [[0 0]] movements)))))
+    (count (houses-visited movements))))
+
+(defn part-2
+  [& [data]]
+  (let [data (or data instructions-from-file)
+        movements (map ->movement (seq data))
+        santa-visits (houses-visited (take-nth 2 movements))
+        robo-santa-visits (houses-visited (take-nth 2 (rest movements)))]
+    (count (distinct (concat santa-visits robo-santa-visits)))))
--- test/advent_of_code/day_3_test.clj
+++ test/advent_of_code/day_3_test.clj
@@ -8,3 +8,10 @@
     ">" 2
     "^>v<" 4
     "^v^v^v^v^v" 2))
+
+(deftest test-part-2
+  (are [input expected-number-of-houses]
+    (= expected-number-of-houses (day-3/part-2 input))
+    "^v" 3
+    "^>v<" 3
+    "^v^v^v^v^v" 11))
