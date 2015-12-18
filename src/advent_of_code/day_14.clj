e41ef3f9ecdc33b3ada0bc35da117bea1d38ed26
Gareth Clay <gareth.clay@gmail.com>
2015-12-18 08:23:35 +0000

    Day 14 part 2

--- src/advent_of_code/day_14.clj
+++ src/advent_of_code/day_14.clj
@@ -21,6 +21,26 @@
         remaining-seconds (mod seconds cycle-duration)]
     (+ (* number-of-cycles speed fly-time) (* speed (min fly-time remaining-seconds)))))
 
+(defn positions
+  [all-reindeer seconds]
+  (partition
+    seconds
+    (for [reindeer all-reindeer
+          second (range 1 (inc seconds))]
+      (distance-traveled second reindeer))))
+
+(defn points
+  [positions]
+  (apply mapv
+         +
+         (apply mapv
+                (fn [& distances]
+                  (let [furthest (apply max distances)]
+                    (map #(if (= furthest %) 1 0) distances)))
+                positions)))
+
+
+
 (defn part-1
   [& [data seconds]]
   (let [all-reindeer (parse-data (or data (load-data "day-14")))
@@ -30,3 +50,13 @@
       (map
         #(vec [(:name %) (int (distance-traveled seconds %))])
         all-reindeer))))
+
+(defn part-2
+  [& [data seconds]]
+  (let [all-reindeer (parse-data (or data (load-data "day-14")))
+        seconds (or seconds 2503)]
+    (sort
+      #(> (get %1 1) (get %2 1))
+      (mapv #(vec [(:name %1) %2])
+            all-reindeer
+            (points (positions all-reindeer seconds))))))
--- test/advent_of_code/day_14_test.clj
+++ test/advent_of_code/day_14_test.clj
@@ -2,14 +2,23 @@
   (:require [advent-of-code.day-14 :as day-14]
             [clojure.test :refer :all]))
 
+(def input
+  (str "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds." \newline
+       "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."))
+
 (deftest part-1
-  (let [input (str "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds." \newline
-                   "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.")]
-    (are [seconds expected-result]
-      (= expected-result (day-14/part-1 input seconds))
-      1    [["Dancer"  16] ["Comet"    14]]
-      10   [["Dancer" 160] ["Comet"   140]]
-      11   [["Dancer" 176] ["Comet"   140]]
-      12   [["Dancer" 176] ["Comet"   140]]
-      137  [["Dancer" 176] ["Comet"   140]]
-      1000 [["Comet" 1120] ["Dancer" 1056]])))
+  (are [seconds expected-result]
+    (= expected-result (day-14/part-1 input seconds))
+    1    [["Dancer"  16] ["Comet"    14]]
+    10   [["Dancer" 160] ["Comet"   140]]
+    11   [["Dancer" 176] ["Comet"   140]]
+    12   [["Dancer" 176] ["Comet"   140]]
+    137  [["Dancer" 176] ["Comet"   140]]
+    1000 [["Comet" 1120] ["Dancer" 1056]]))
+
+(deftest part-2
+  (are [seconds expected-result]
+    (= expected-result (day-14/part-2 input seconds))
+    1    [["Dancer"   1] ["Comet"   0]]
+    140  [["Dancer" 139] ["Comet"   1]]
+    1000 [["Dancer" 689] ["Comet" 312]]))
