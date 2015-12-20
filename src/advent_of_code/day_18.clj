b783562753d13bec6dee0c918b672edc74b384ac
Gareth Clay <gareth.clay@gmail.com>
2015-12-20 10:28:17 +0000

    Day 18 part 2

--- src/advent_of_code/day_18.clj
+++ src/advent_of_code/day_18.clj
@@ -13,23 +13,38 @@
       (nth y [])
       (nth x false)))
 
-(defn next-state
-  [lights [x y :as coords]]
-  (let [neighbours (for [nx (range (dec x) (+ 2 x))
-                         ny (range (dec y) (+ 2 y))
-                         :when (not (= [nx ny] [x y]))]
-                     [nx ny])
-        neighbours-switched-on (count (filter true? (map (partial currently-on? lights)
-                                                         neighbours)))]
+(defn neighbours
+  [[x y]]
+  (for [nx (range (dec x) (+ 2 x))
+        ny (range (dec y) (+ 2 y))
+        :when (not (= [nx ny] [x y]))]
+    [nx ny]))
+
+(defn neighbours-switched-on
+  [lights coords]
+  (count (filter true? (map (partial currently-on? lights) (neighbours coords)))))
+
+(defn next-state-part-1
+  [lights coords]
+  (let [neighbours-switched-on (neighbours-switched-on lights coords)]
     (if (currently-on? lights coords)
       (number? (some #{2 3} [neighbours-switched-on]))
       (= 3 neighbours-switched-on))))
 
+(defn next-state-part-2
+  [lights coords]
+  (let [permanently-on (set (for [y [0 (dec (count lights))]
+                                  x [0 (dec (count (get lights y)))]]
+                              [x y]))]
+    (if (some permanently-on [coords])
+      true
+      (next-state-part-1 lights coords))))
+
 (defn update-lights
-  [lights]
-  (vec (for [y (range 0 (count lights))]
-         (vec (for [x (range 0 (count (get lights y)))]
-                (next-state lights [x y]))))))
+  [next-state-fn lights]
+  (vec (for [y (range (count lights))]
+         (vec (for [x (range (count (get lights y)))]
+                (next-state-fn lights [x y]))))))
 
 (defn render
   [light-sequence]
@@ -39,9 +54,30 @@
             (println (apply str chars)))
     (println)))
 
+(defn count-switched-on
+  [lights]
+  (reduce (fn [sum row] (+ sum (count (filter true? row)))) 0 lights))
+
 (defn part-1
   [& [data steps]]
   (let [initial-state (parse-data (or data (load-data "day-18")))
         steps (or steps 100)
-        light-sequence (take (inc steps) (iterate update-lights initial-state))]
-    (reduce (fn [sum row] (+ sum (count (filter true? row)))) 0 (last light-sequence))))
+        final-state (last (take (inc steps)
+                                (iterate (partial update-lights next-state-part-1) initial-state)))]
+    (count-switched-on final-state)))
+
+(defn part-2
+  [& [data steps]]
+  (let [initial-state (parse-data (or data (load-data "day-18")))
+        corners (for [y [0 (dec (count initial-state))]
+                      x [0 (dec (count (get initial-state y)))]]
+                  [y x])
+        corrected-state (loop [state initial-state
+                               [corner & corners] corners]
+                          (if corner
+                            (recur (update-in state corner (constantly true)) corners)
+                            state))
+        steps (or steps 100)
+        final-state (last (take (inc steps)
+                                (iterate (partial update-lights next-state-part-2) corrected-state)))]
+    (count-switched-on final-state)))
--- test/advent_of_code/day_18_test.clj
+++ test/advent_of_code/day_18_test.clj
@@ -11,3 +11,6 @@
 
 (deftest test-part-1
   (is (= 4 (day-18/part-1 data 4))))
+
+(deftest test-part-2
+  (is (= 17 (day-18/part-2 data 5))))
